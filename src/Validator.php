90c8f4cfc272cfb32765bf8d388dc8c319209263
Łukasz Sitnicki <lukasz.sitnicki@gmail.com>
2019-04-11 08:46:49 +0200

    Validator declaration fix

--- src/Validator.php
+++ src/Validator.php
@@ -25,7 +25,7 @@ abstract class Validator
      *
      * @throws InvariantViolation
      */
-    abstract public function protectInvariant(): void {}
+    abstract public function protectInvariant(): void;
 
     /**
      * Return attribute of ValueObject.
